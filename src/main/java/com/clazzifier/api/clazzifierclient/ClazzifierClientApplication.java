21d85c4d58c889f5a9b62896d239da8961ab335c
Abhash Shrivastava <abhash.shrivastav.ext@bharatpe.com>
2022-03-05 09:53:17 +0530

    initial commit by AS

---
+++ src/main/java/com/clazzifier/api/clazzifierclient/ClazzifierClientApplication.java
@@ -0,0 +1,13 @@
+package com.clazzifier.api.clazzifierclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClazzifierClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClazzifierClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clazzifier/api/clazzifierclient/ClazzifierClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clazzifier.api.clazzifierclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClazzifierClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
